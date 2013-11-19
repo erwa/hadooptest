6c43f9f8fac93509a6dfe7ffb3041a3dba3687a0
Anthony Hsu <erwaman@gmail.com>
2013-11-19 13:25:59 -0800

    Added UrlDayIp map-reduce implementation and test input, .classpath, .project files

---
+++ UrlDayIp.java
@@ -0,0 +1,75 @@
+//package test;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.MapReduceBase;
+
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class UrlDayIp {
+    public static class MyMap extends MapReduceBase implements Mapper<LongWritable,Text,Text,Text> {
+        private Text urlip = new Text();
+        private Text day = new Text();
+
+        public void map(LongWritable k, Text v, OutputCollector<Text,Text> output, Reporter reporter) throws IOException {
+            String line = v.toString();
+            String[] words = line.split(",");
+            String url = words[0];
+            
+            day.set(words[1]);
+
+            for (int i = 2; i < words.length; i++) {
+                urlip.set(url + "," + words[i]);
+                output.collect(urlip,day); 
+            }
+        }
+    }
+
+    public static class MyReduce extends MapReduceBase implements Reducer<Text,Text,Text,Text> {
+       private Text daysVal = new Text(); 
+
+        public void reduce(Text k, Iterator<Text> vals, OutputCollector<Text,Text> output, Reporter r) throws IOException {
+            String days = vals.next().toString();
+            while (vals.hasNext()) {
+                days += ","+vals.next().toString();
+            
+            }
+            daysVal.set(days);
+            output.collect(k,daysVal);
+        }
+    }
+
+
+    public void run(String[] args) throws Exception {
+       JobConf conf = new JobConf(getClass());
+
+        conf.setJarByClass(getClass());
+       conf.setJobName(getClass().getCanonicalName());
+       conf.setMapperClass(UrlDayIp.MyMap.class);
+       conf.setReducerClass(UrlDayIp.MyReduce.class);
+
+       conf.setMapOutputKeyClass(Text.class);
+       conf.setOutputKeyClass(Text.class);
+
+       FileInputFormat.setInputPaths(conf, new Path(args[0]));
+       FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+       JobClient.runJob(conf);
+    }
+
+	public static void main(String[] args) throws Exception {
+        UrlDayIp udi = new UrlDayIp();
+        udi.run(args);
+    }
+}
---
+++ UrlDayIpReducer.java
@@ -0,0 +1,34 @@
+//package test;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.MapReduceBase;
+
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+    public class UrlDayIpReducer extends MapReduceBase implements Reducer<Text,Text,Text,Text> {
+       private Text daysVal = new Text(); 
+
+        public void reduce(Text k, Iterator<Text> vals, OutputCollector<Text,Text> output, Reporter r) throws IOException {
+            String days = vals.next().toString();
+            while (vals.hasNext()) {
+                days += ","+vals.next().toString();
+            
+            }
+            daysVal.set(days);
+            output.collect(k,daysVal);
+        }
+    }
+
+
